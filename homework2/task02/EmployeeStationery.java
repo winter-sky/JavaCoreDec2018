fa0de3678aa57af64c8f5c96e6750d9b14428827
winter-sky <skazo4nayatayga@gmail.com>
2019-02-13 03:29:51 +0300

    added createEmployeeStationery and ChooseStationeryType method to EmployeeStationery class

--- homework2/task02/EmployeeStationery.java
+++ homework2/task02/EmployeeStationery.java
@@ -4,11 +4,30 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class EmployeeStationery {
-    Map<Employee,Stationery> empStationery = new TreeMap<Employee,Stationery>();
-    public void addStationery(){
+    Map<Employee, Stationery> empStationery = new TreeMap<Employee, Stationery>();
+
+    public void createEmployeeStationery() {
+        this.chooseStationeryType();
+    }
+
+    public void chooseStationeryType() {
+        int i = 1;
+        for (StationeryType type : StationeryType.values()) {
+            System.out.println("Enter " + i + " to add " + type.toString());
+            i++;
+        }
+    }
+
+    public void addStationery() {
 
     }
-    public void deleteStationery(){
 
+    public void deleteStationery() {
+
+    }
+
+    public static void main(String[] args) {
+        EmployeeStationery employeeStationery = new EmployeeStationery();
+        employeeStationery.createEmployeeStationery();
     }
 }
