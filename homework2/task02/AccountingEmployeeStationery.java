c4bb70b8e624becf9925f07ee132c3c01ecb576b
winter-sky <skazo4nayatayga@gmail.com>
2019-02-14 03:46:07 +0300

    deleted commented code

--- homework2/task02/AccountingEmployeeStationery.java
+++ homework2/task02/AccountingEmployeeStationery.java
@@ -6,10 +6,6 @@ import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static homework2.task02.Department.IT;
-import static homework2.task02.StationeryType.NOTEBOOK;
-import static homework2.task02.StationeryType.PEN;
-
 public class AccountingEmployeeStationery {
     TreeMap<Employee, EmployeeStationery> empStationery = new TreeMap<Employee, EmployeeStationery>();
 
@@ -50,40 +46,8 @@ public class AccountingEmployeeStationery {
         }
     }
 
-
     public static void main(String[] args) {
-//        Employee employee = new Employee("Kirill", "Romanov", IT);
-//        employee.setId(1);
-//        EmployeeStationery employeeStationery = new EmployeeStationery();
-//        employeeStationery.addStationery(300,"SomeBrand", PEN);
-//        employeeStationery.addStationery(60,"SomeBrand",StationeryType.NOTEBOOK);
         AccountingEmployeeStationery accountingEmployeeStationery = new AccountingEmployeeStationery();
-//        accountingEmployeeStationery.empStationery.put(employee,employeeStationery);
-//        employeeStationery.addStationery(50, "AnotherBrand", StationeryType.NOTEBOOK);
-//
-//        Employee emp = new Employee("Vasya","Ivanov", IT);
-//        EmployeeStationery employeeStationery1 = new EmployeeStationery();
-//        employeeStationery1.addStationery(80, "BrandName", PEN);
-//        accountingEmployeeStationery.empStationery.put(emp,employeeStationery1);
-//
-//        Employee emp2 = new Employee("Alisa","Taiga", IT);
-//        EmployeeStationery employeeStationery2 = new EmployeeStationery();
-//        employeeStationery1.addStationery(700, "BrandName", NOTEBOOK);
-//        accountingEmployeeStationery.empStationery.put(emp2,employeeStationery2);
-
-//        for(Map.Entry entry:accountingEmployeeStationery.empStationery.entrySet()){
-//            System.out.println(entry.getKey().toString());
-//            EmployeeStationery e = (EmployeeStationery) entry.getValue();
-//            System.out.println(e.employeeStationery);
-//        }
-
-//        accountingEmployeeStationery.showEmployees();
-//        try {
-//            accountingEmployeeStationery.showEmployeeStationery();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         try {
             accountingEmployeeStationery.createAccounting();
         } catch (IOException e) {
