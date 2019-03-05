257fe8fe3c30fcd98e29282271986701333b871e
winter-sky <skazo4nayatayga@gmail.com>
2019-03-05 03:05:07 +0300

    added task 01

---
+++ src/main/java/homework1/task01/Note.java
@@ -0,0 +1,45 @@
+package homework1.task01;
+
+/**
+ *This class creates a new notepad entry
+ */
+public class Note {
+    private int id;
+    private String content;
+
+    public Note(String note){
+        content = note;
+    }
+
+    /**
+     * Sets an id for a new entry.
+     * @param id
+     */
+    public void setId(int id){
+        this.id=id;
+    }
+
+    /**
+     * Retrieves the id of an existing entry.
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Retrieves the contents of the notepad entry
+     * @return content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Set the contents of the notepad entry
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
